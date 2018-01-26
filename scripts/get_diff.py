a2457bf2027fde4185d12072030939d6dac0934a
Shamil Chollampatt <shamil.cm@gmail.com>
2018-01-26 21:40:24 +0800

    added scripts

---
+++ scripts/get_diff.py
@@ -0,0 +1,13 @@
+import sys
+
+
+prefix = sys.argv[1]
+src = sys.argv[2]
+trg = sys.argv[3]
+
+with open(prefix + '.' + src) as f_src, open(prefix + '.' + trg) as f_trg:
+    for sline, tline in zip(f_src, f_trg):
+        sline = sline.strip()
+        tline = tline.strip()
+        if sline != tline:
+            print sline+'\t'+tline
---
+++ scripts/nbest_reformat.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input-file',  help='path to input file (output of fairseq)')
+parser.add_argument('--debpe',  action='store_true', help='enable the flag to post-process and remove BPE segmentation.')
+
+args = parser.parse_args()
+
+
+scount = -1
+with open(args.input_file) as f:
+    for line in f:
+        line = line.strip()
+        pieces = line.split('\t')
+        if pieces[0] == 'S':
+            scount += 1
+        if pieces[0] == 'H':
+            hyp = pieces[2]
+            if args.debpe:
+                hyp = hyp.replace('@@ ','')
+            score = pieces[1]
+            print("%d ||| %s ||| F0= %s ||| %s" % (scount, hyp, score, score) )
+
+

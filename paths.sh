8813b9a97e16f30bab32e2112e8e03a8855609c1
Shamil Chollampatt <shamil.cm@gmail.com>
2018-01-26 21:44:30 +0800

    added paths file

---
+++ paths.sh
@@ -0,0 +1,7 @@
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"  && pwd )"
+DATA_DIR=$BASE_DIR/data
+MODEL_DIR=$BASE_DIR/models
+SCRIPTS_DIR=$BASE_DIR/scripts
+SOFTWARE_DIR=$BASE_DIR/software
+
+

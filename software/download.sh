95725921fcdc7604a15aad0f9781d0cd9c902400
Shamil Chollampatt <shamilcm@users.noreply.github.com>
2018-06-11 17:18:15 +0800

    Update download.sh

--- software/download.sh
+++ software/download.sh
@@ -1,14 +1,16 @@
-echo "Downloading Fairseq from https://github.com/shamilcm/fairseq-py (rev:90c31cd92055124c427689c00624b1eb84c5688a)"
-wget https://github.com/shamilcm/fairseq-py/archive/90c31cd92055124c427689c00624b1eb84c5688a.zip 
-unzip 90c31cd92055124c427689c00624b1eb84c5688a.zip
-rm 90c31cd92055124c427689c00624b1eb84c5688a.zip
-mv fairseq-py-90c31cd92055124c427689c00624b1eb84c5688a fairseq-py
+COMMIT=ceb2f1200c9e5b8bf42a1033e7638d3e8586609a
+echo "Downloading Fairseq from https://github.com/shamilcm/fairseq-py (rev:$COMMIT)"
+wget https://github.com/shamilcm/fairseq-py/archive/$COMMIT.zip
+unzip $COMMIT.zip
+rm $COMMIT.zip
+mv fairseq-py-$COMMIT fairseq-py
 
-echo "Downloading n-best reranker from https://github.com/nusnlp/nbest-reranker (rev: 454c4adc90d0469ef7b2c71ff8cf849ea8cb67f)"
-wget https://github.com/nusnlp/nbest-reranker/archive/454c4adc90d0469ef7b2c71ff8cf849ea8cb67f6.zip
-unzip 454c4adc90d0469ef7b2c71ff8cf849ea8cb67f6.zip
-rm 454c4adc90d0469ef7b2c71ff8cf849ea8cb67f6.zip
-mv nbest-reranker-454c4adc90d0469ef7b2c71ff8cf849ea8cb67f6 nbest-reranker
+COMMIT=b9453d5a211fc8f90fb25a584b39d4784f8de716
+echo "Downloading n-best reranker from https://github.com/nusnlp/nbest-reranker (rev: $COMMIT)"
+wget https://github.com/nusnlp/nbest-reranker/archive/$COMMIT.zip
+unzip $COMMIT.zip
+rm $COMMIT.zip
+mv nbest-reranker-$COMMIT nbest-reranker
 #git clone https://github.com/nusnlp/nbest-reranker/
 
 echo "Downloading Subword NMT from https://github.com/rsennrich/subword-nmt (rev: ec5c7b009c409e72b5ef65a77c1a846546f14847)"
